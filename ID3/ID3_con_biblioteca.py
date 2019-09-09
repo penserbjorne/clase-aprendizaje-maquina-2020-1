4c0d4ace30a4cebbf5335c49623c6d61413c9178
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2019-09-09 00:22:18 -0500

    Se realizo ID3 con biblioteca utilizando el dataset de diabetes de sklearn

--- ID3/ID3_con_biblioteca.py
+++ ID3/ID3_con_biblioteca.py
@@ -1,70 +1,69 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[34]:
+# In[146]:
 
 
+from sklearn.datasets import load_diabetes
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn import metrics
 from id3 import Id3Estimator, export_graphviz
 import numpy as np
 import graphviz
 
 
-# In[35]:
+# In[147]:
 
 
-feature_names = ["color",
-                 "forma",
-                 "tamanio"]
+data = load_diabetes()
 
 
-# In[36]:
+# In[148]:
 
 
-x = np.array([["rojo", "cuadrado", "grande"],
-              ["azul", "cuadrado", "grande"],
-              ["rojo", "redondo", "pequenio"],
-              ["verde", "cuadrado", "pequenio"],
-              ["rojo", "redondo", "grande"],
-              ["verde", "cuadrado", "grande"]])
+size = int(data.data.shape[0]*0.8)
+data_t = np.c_[data.data, data.target]
 
 
-# In[37]:
+# In[149]:
 
 
-y = np.array(["+",
-              "+",
-              "-",
-              "-",
-              "+",
-              "-"])
+x_test = data.data[:size, :]
+x_train = data.data[size:, :]
+y_test = data.target[:size]
+y_train = data.target[size:]
 
 
-# In[38]:
+# In[150]:
 
 
 id3 = Id3Estimator()
+id3.fit(x_train, y_train)
+y_pred = id3.predict(x_test)
 
 
-# In[39]:
+# In[151]:
 
 
-id3.fit(x, y)
+print('Tamaño de test: ',size)
+print("Precisión:",metrics.accuracy_score(y_test, y_pred))
+print(precision_recall_fscore_support(y_test, y_pred))
 
 
-# In[40]:
+# In[152]:
 
 
-export_graphviz(id3.tree_, 'arbol.dot', feature_names)
+export_graphviz(id3.tree_, 'arbol.dot', data.feature_names)
 
 
-# In[41]:
+# In[153]:
 
 
 with open("arbol.dot") as f:
     dot_graph = f.read()
 
 
-# In[43]:
+# In[154]:
 
 
 g = graphviz.Source(dot_graph)
--- ID3/README.md
+++ ID3/README.md
@@ -4,7 +4,7 @@
 
 Se utilizaron los paquetes:
 
-- sklearn.
+- sklearn
 - numpy
 - decision-tree-id3
 - graphviz
@@ -17,9 +17,6 @@ Es necesario instalar en los siguientes niveles.
 
 Para ejecutar el programa puede abrir el notebook `ID3_con_biblioteca.ipynb` o ejecutar directamente en la terminal `python ID3_con_biblioteca.py`.
 
-El set de datos utilizado es del [Registro Nacional de Datos de Personas Extraviadas o Desaparecidas (RNPED)](https://datos.gob.mx/busca/dataset/registro-nacional-de-datos-de-personas-extraviadas-o-desaparecidas-rnped)
-
-- [Base de datos del RNPED del fuero común](https://datos.gob.mx/busca/dataset/registro-nacional-de-datos-de-personas-extraviadas-o-desaparecidas-rnped/resource/57465b84-d7fb-4ff5-9761-747316b01786)
-- [Base de datos del RNPED del fuero federal](https://datos.gob.mx/busca/dataset/registro-nacional-de-datos-de-personas-extraviadas-o-desaparecidas-rnped/resource/a14b4be3-ab83-457c-b1ff-7e56e859da89)
+El set de datos utilizado es de [diabetes de scikit-learn](https://scikit-learn.org/stable/datasets/index.html#diabetes-dataset).
 
 La salida es un PDF con el árbol correspondiente a ID3.
